b6940c4a0eccabb659dba9ab9a803f868e268c07
Mikhail Apurin <m_apurin@yumemi.co.jp>
2023-07-22 00:58:42 +0900

    Initialize SwiftPM library

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version: 5.8
+
+import PackageDescription
+
+let package = Package(
+    name: "Destinations",
+    products: [
+        .library(
+            name: "Destinations",
+            targets: ["Destinations"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "Destinations"),
+    ]
+)
